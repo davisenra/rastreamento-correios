13e97a8b22c7865104c7d245013106fde612c861
Davi Martins <davi.senra98@gmail.com>
2022-06-13 21:38:10 -0300

    Refactor

--- correios.py
+++ correios.py
@@ -1,35 +1,52 @@
+import requests
 import json
 from sys import argv
-from api_request import api_request
+from typing import Dict
 
 
-def rastreamento(cod_objeto):
-    api_request(cod_objeto)
+def main():
+    if (len(argv) != 2):
+        print(f"Utilização: python correios.py [código do objeto]")
+    else:
+        track_object(api_request(argv[1]))
 
-    output = open("search_output.json")
-    data = json.load(output)
+    return
 
-    objeto = data["objetos"][0]["codObjeto"]
+
+def api_request(object_code: str) -> Dict:
+    req = requests.get(
+        url="https://proxyapp.correios.com.br/v1/sro-rastro/"+object_code)
+    r = req.json()
+
+    return r
+
+
+def track_object(json_response: Dict):
+    data = json_response
+    tracked_object = data["objetos"][0]["codObjeto"]
     tipo_postal = data["objetos"][0]["tipoPostal"]["categoria"]
+    event = data["objetos"][0]["eventos"][0]
 
-    print(f"Código do objeto: {objeto}")
+    print(f"Código do objeto: {tracked_object}")
     print(f"Tipo postal: {tipo_postal}")
 
-    eventos = data["objetos"][0]["eventos"][0]
     i = 0
-    for evento in eventos:
-        status_evento = data["objetos"][0]["eventos"][i]["descricao"]
-        data_evento = data["objetos"][0]["eventos"][i]["dtHrCriado"][:10]
-        hora_evento = data["objetos"][0]["eventos"][i]["dtHrCriado"][11:16]
+    for each_event in event:
+        event_status = data["objetos"][0]["eventos"][i]["descricao"]
+        event_date = data["objetos"][0]["eventos"][i]["dtHrCriado"][:10]
+        event_hour = data["objetos"][0]["eventos"][i]["dtHrCriado"][11:16]
+        event_city = data["objetos"][0]["eventos"][i]["unidade"]["endereco"]["cidade"]
+        event_state = data["objetos"][0]["eventos"][i]["unidade"]["endereco"]["uf"]
 
         print(f"")
-        print(f"Status: {status_evento}")
-        print(f"Data: {data_evento}")
-        print(f"Hora: {hora_evento}")
+        print(f"Status: {event_status}")
+        print(f"Data: {event_date}")
+        print(f"Hora: {event_hour}")
+        print(f"Cidade/Estado: {event_city}/{event_state}")
         i += 1
 
     return 0
 
 
 if __name__ == "__main__":
-    rastreamento(argv[1])
+    main()
